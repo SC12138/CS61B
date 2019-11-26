3f91eecf96cadeac30db551b04bdae7da08dc05e
SongChen <song.chen.628@gmail.com>
2019-11-26 16:19:11 +0800

    Complete hw2 ver1.0

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,86 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    boolean[][] open;
+    WeightedQuickUnionUF union;
+    private int FULL;
+    private int size;
+    private int LOWERBOUND;
+    private int openSite;
+    public Percolation(int N) {               // create N-by-N grid, with all sites initially blocked
+        union = new WeightedQuickUnionUF(N*N+2); //N*N refers to water, N*N+1 refers to lower bound
+        open = new boolean[N][N];
+        size = N;
+        FULL = N*N;
+        LOWERBOUND = N*N+1;
+        openSite = 0;
+        for (boolean[] subArray: open){
+            for (boolean o: subArray){
+                o=false;
+            }
+        }
+    }
 
+    private int xyToInt(int row, int col){
+        if (row<0 || row>=size || col<0 || col>=size){
+            throw new RuntimeException("Invalid site position");
+        }
+        return row*this.size + col;
+    }
+
+    public void open(int row, int col) {       // open the site (row, col) if it is not open already
+        if (isOpen(row, col)){
+            return;
+        }
+        open[row][col] = true;
+        openSite += 1;
+        int siteIndex = xyToInt(row, col);
+        // connect with sites nearby
+        if(row>0 && isOpen(row-1, col)){ // up
+            union.union(siteIndex, xyToInt(row-1, col));
+        }
+        if(row<size-1 && isOpen(row+1, col)){ // down
+            union.union(siteIndex, xyToInt(row+1, col));
+        }
+        if(col>0 && isOpen(row, col-1)){ //left
+            union.union(siteIndex, xyToInt(row, col-1));
+        }
+        if(col<size-1 && isOpen(row, col+1)) { //right
+            union.union(siteIndex, xyToInt(row, col+1));
+        }
+        // if site is on the upper bound
+        if (row == 0){
+            union.union(siteIndex, FULL);
+        }
+        // if site is on the lower bound
+        if (row == size-1){
+            union.union(siteIndex, LOWERBOUND);
+        }
+    }
+
+
+    public boolean isOpen(int row, int col) {  // is the site (row, col) open?
+        return (open[row][col] == true);
+    }
+
+
+    public boolean isFull(int row, int col) {  // is the site (row, col) full?
+        int siteIndex = xyToInt(row, col);
+        return union.connected(siteIndex, FULL);
+    }
+
+
+    public int numberOfOpenSites() {          // number of open sites
+        return this.openSite;
+    }
+
+
+    public boolean percolates() {              // does the system percolate?
+        return (union.connected(FULL, LOWERBOUND));
+    }
+
+
+    public static void main(String[] args){   // use for unit testing (not required)
+
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,69 @@
 package hw2;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+import java.util.List;
+import java.util.ArrayList;
 
 public class PercolationStats {
+    private double[] fracToParco;
+    private int numOfExperi;
+    private int size;
 
+    public PercolationStats(int N, int T, PercolationFactory pf) {   // perform T independent experiments on an N-by-N grid
+        if (N<=0 || T <=0) {throw new IllegalArgumentException("N and T should be positive");}
+        size = N;
+        numOfExperi = T;
+        fracToParco = new double[numOfExperi];
+        for (int i=0; i<T; i++){ // perform T experiments
+            //Percolation p = pf.make(size);
+            Percolation p = new Percolation(size);
+            // construct an index list
+            List<Integer> indexes = new ArrayList<>(size * size);
+            int unopened = size * size; //
+            for (int ind=0; ind < unopened; ind++){
+                indexes.add(ind);
+            }
+            // uniformly get random index from the list
+            for (int j=0; j<size*size; j++){
+                int randInd = (int)Math.floor(StdRandom.uniform(unopened)); // generate a random index of the index list
+                int openIndex = indexes.remove(randInd); // get the random index of the tile to open
+                int temCol = openIndex % this.size;
+                int temRow = (openIndex - temCol)/size;
+                // open the tile
+                p.open(temRow, temCol);
+                unopened -= 1; // update number of unopened tiles
+                if (p.percolates()){
+                    this.fracToParco[i] = (j+1)/Math.pow(this.size, 2);
+                    break;
+                }
+
+            }
+        }
+    }
+
+
+
+    public double mean() {                                          // sample mean of percolation threshold
+        return StdStats.mean(this.fracToParco);
+    }
+
+    public double stddev() {                                         // sample standard deviation of percolation threshold
+        return StdStats.stddev(this.fracToParco);
+    }
+
+    public double confidenceLow() {                              // low endpoint of 95% confidence interval
+        return (this.mean() - (1.96*this.stddev())/Math.sqrt(numOfExperi));
+    }
+
+
+    public double confidenceHigh() {                                 // high endpoint of 95% confidence interval
+        return (this.mean() + (1.96*this.stddev())/Math.sqrt(numOfExperi));
+    }
+
+    public static void main(String[] args){
+        PercolationStats pS = new PercolationStats(20, 30, null);
+        System.out.println(pS.mean());
+        System.out.println(pS.stddev());
+    }
 }
