8cae6570b969c89fd4eb22652407e57b68d3cbed
SongChen <song.chen.628@gmail.com>
2019-12-12 21:39:59 +0800

    Reverse output order

--- lab13/RadixSort.java
+++ lab13/RadixSort.java
@@ -4,7 +4,9 @@ import org.junit.Test;
 import java.awt.image.Raster;
 import java.time.chrono.JapaneseEra;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 
 /**
@@ -31,15 +33,66 @@ public class RadixSort {
                 maxLen = s.length();
             }
         }
+
+
         String[] sort = new String[asciis.length];
         System.arraycopy(asciis, 0, sort,0, asciis.length);
         for (int i = maxLen-1 ; i>=0;i-=1){
             RadixSort.sortHelperLSD(sort, i);
         }
+
+
         return sort;
 
     }
+    /*
+    public static String[] sort(String[] asciis) {
+        Queue<String>[] buckets = new Queue[256];
+        for (int i = 0; i < 256; i++)
+            buckets[i] = new LinkedList();
 
+        boolean sorted = false;
+        int lengthInc = 0;
+
+        String[] sortedArr = new String[asciis.length];
+        System.arraycopy(asciis, 0, sortedArr, 0, asciis.length);
+
+        while (!sorted) {
+            sorted = true;
+
+            for (String item : sortedArr) {
+                int index = item.length() - lengthInc - 1;
+                if (index >= 0) {
+                    sorted = false;
+                    int ofASCII = (int) item.charAt(index);
+                    buckets[ofASCII].add(item);
+                } else {
+                    buckets[(int) item.charAt(0)].add(item);
+                }
+
+
+            }
+
+            lengthInc++;
+            int index = 0;
+
+            for (Queue<String> bucket : buckets) {
+                while (!bucket.isEmpty()) {
+                    sortedArr[index] = bucket.remove();
+                    index++;
+                }
+            }
+
+            //System.out.println();
+
+        }
+
+        //System.out.println("");
+
+        return sortedArr;
+    }
+
+     */
     /**
      * LSD helper method that performs a destructive counting sort the array of
      * Strings based off characters at a specific index.
@@ -48,11 +101,7 @@ public class RadixSort {
      */
     private static void sortHelperLSD(String[] asciis, int index) {
 
-        for (int i=0; i<asciis.length/2; i++){
-            String temp = asciis[i];
-            asciis[i] = asciis[asciis.length - i - 1];
-            asciis[asciis.length - i -1] = temp;
-        }
+
 
         int[] counts = new int[257]; // last index for String that is shorter
         int[] posit = new int[257];
@@ -85,11 +134,7 @@ public class RadixSort {
         System.arraycopy(sorted, sorted.length - counts[256], asciis, 0, counts[256]);
         System.arraycopy(sorted, 0, asciis, counts[256], sorted.length - counts[256]);
 
-        for (int i=0; i<asciis.length/2; i++){
-            String temp = asciis[i];
-            asciis[i] = asciis[asciis.length - i - 1];
-            asciis[asciis.length - i -1] = temp;
-        }
+
     }
 
     /**
@@ -107,19 +152,30 @@ public class RadixSort {
         return;
     }
 
-    /*
+
+
     @Test
     public void testRadix(){
-        String[] ts = new String[]{"abc", "cde", "abe", "efgh", "f", " ",""};
-        RadixSort.sortHelperLSD(ts, 0);
-        String[] expected = new String[]{"f", "efgh", "cde", "abc", "abe", " ", ""};
+        String[] ts = new String[]{"132","93" ,"155" ,"242","118", "26","204"};
+        RadixSort.sortHelperLSD(ts, 2);
+        String[] expected = new String[]{"93","26","132", "242", "204", "155",  "118"};
         assertArrayEquals(expected, ts);
 
-        ts = new String[]{"abe", "cde", "abc", "efgh", "f", " ",""};
-        expected = new String[]{"f", "efgh", "cde", "abe", "abc", " ", ""};
+        ts = new String[]{"132","93" ,"155" , "242","118", "26","204"};
+        expected = new String[]{"118", "132" ,"155" ,"204" ,"242", "26", "93"};
         String[] result = RadixSort.sort(ts);
+        for (String s:result){
+            System.out.println(s);
+        }
         assertArrayEquals(expected, result);
+
+        ts = new String[]{"118", "155","93", "132", "1", "262","24",};
+        expected = new String[]{"1","118", "132", "155","24","262","93"};
+        result = RadixSort.sort(ts);
+        assertArrayEquals(expected, result);
+
+
     }
 
-     */
+
 }
